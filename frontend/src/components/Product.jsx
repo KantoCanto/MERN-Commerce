2235138ebb4c093531f028400e34ec65a08512aa
KantoCanto <bruno.canto.neves@gmail.com>
2023-10-06 12:11:12 +0100

    added react-router and react-router-bootstrap

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Card, CardTitle } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <CardTitle as="div">
             <strong>{product.name}</strong>
           </CardTitle>
-        </a>
+        </Link>
 
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./assets/styles/index.css";
 import "./assets/styles/bootstrap.custom.css";
 //import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import HomeScreen from "./screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
